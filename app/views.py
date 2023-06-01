72e53958887d4811ca82d70a9a7fdfdc3faa1ab4
Vishnureddy123456 <vishnureddylekkala@gmail.com>
2023-06-01 13:46:58 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def srujana(request):
+    return render(request,'srujana.html')
+def vishnu(request):
+    return render(request,'vishnu.html')
